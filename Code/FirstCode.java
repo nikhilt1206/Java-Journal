252ed889af6caef6f4a6cd69b1e8a6386bcbae60
Nikhil Tiwari <nikhiltiwari@192.168.1.16>
2026-08-11 00:14:45 +0530

    Added code for Lecture-4

---
+++ Code/FirstCode.java
@@ -0,0 +1,14 @@
+package Code;
+
+public class FirstCode {
+    public static void main(String[] args){
+
+        int a;
+        a = 20;
+        long x = 2000;
+        char gender = 'm';
+        float percentage = 33.23f;
+        boolean isPresent = false;
+
+    }
+}
